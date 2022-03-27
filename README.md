9f8b3c3b42204f62951b31f832b64f8c14e014cd
sanyi0411 <sandor.derda@gmail.com>
2022-03-27 21:05:21 +0200

    Create environment

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Asteroids
 Arcade asteroids game written in C++, using SDL
+
+## Prerequisites
+
+ - `cmake` installed on your computer
+ - Internet connection:
+     - This project uses the SDL2 and SDL_image libraries for graphics and the gtest libraries for testing
+     - These libraries are open source
+     - It would redundant to include them in this repo so they are included as submodules
+
+## Build
+
+ - Clone this repository with `git clone --recurse-submodules` to also clone the submodules
+ - `cd` into the folder
+ - Run `cd build`
+ - Run `cmake ..` to configure
+ - Run `cmake --build .` to build the binaries
+ - The executable, called `Asteroids` will be in the _build/bin_ folder, along with the necessary .dll-s
+
+## Tests
+
+ - Tested on Windows 10
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Game.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+
+    if(SDL_Init(SDL_INIT_VIDEO) > 0)
+    {
+        std::cout << "SDL_init failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+    }
+
+    if(!(IMG_Init(IMG_INIT_PNG)))
+    {
+        std::cout << "IMG_init failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("My first window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    return 0;
+}
